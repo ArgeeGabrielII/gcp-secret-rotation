1a9681b6bb5bbb9ff6baa39bb59711cde5a9fa78
Rosalito gabriel <rosalitogabriel@solaireresort.com>
2024-05-01 23:51:22 +0800

    Updated Routes

--- src/jwt-token/jwt-token.controller.ts
+++ src/jwt-token/jwt-token.controller.ts
@@ -3,21 +3,20 @@ import { JwtTokenService } from './jwt-token.service';
 import { CreateTokenDto } from './dto/create-token.dto';
 import { GetTokenDto } from './dto/get-token.dto';
 import { JWTToken, JWTVerify } from './jwt-token.model';
-import { IsPublic } from 'src/decorator/is-public.decorator';
 
-@Controller('/ofm')
+@Controller('/v1')
 export class JwtTokenController {
-    constructor(private jwtTokenService: JwtTokenService) {}
-    @IsPublic()
-    @Post('/generate-token')
+    constructor(private svcJWTToken: JwtTokenService) {}
+
+    @Post('/token/generate')
     getToken(@Body() createTokenDto: CreateTokenDto): JWTToken {
         console.log(`[GENERATE JWT] createTokenDto: ${JSON.stringify(createTokenDto)}`);
-        return this.jwtTokenService.generateToken(createTokenDto);
+        return this.svcJWTToken.generateToken(createTokenDto);
     }
-    @IsPublic()
-    @Get('/verify-token')
+
+    @Get('/token/verify')
     verifyToken(@Headers() getTokenDto: GetTokenDto): JWTVerify {
         console.log(`[VERIFY JWT] getTokenDto: ${JSON.stringify(getTokenDto)}`);
-        return this.jwtTokenService.validateToken(getTokenDto);
+        return this.svcJWTToken.validateToken(getTokenDto);
     }
 }
--- src/secret-manager/secret-manager.controller.ts
+++ src/secret-manager/secret-manager.controller.ts
@@ -1,11 +1,11 @@
 import { Body, Controller, Get } from '@nestjs/common';
 import { SecretManagerService } from './secret-manager.service';
 
-@Controller('secret-manager')
+@Controller('v1')
 export class SecretManagerController {
     constructor(private svcSecret: SecretManagerService) {}
 
-    @Get('/')
+    @Get('/secret')
     async loadSecrets(@Body() names: string[]) {
         console.log(`[NAMES ARRAY INPUT] ${names}`);
         return this.svcSecret.getSecrets(names);
